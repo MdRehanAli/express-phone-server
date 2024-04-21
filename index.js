e40f05c0666794d453a7e3ba0224657a304c483e
MdRehanAli <rehanali21d@gmail.com>
2024-04-22 01:42:25 +0600

    Added phone.json and load data

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const express = require('express')
+const phones = require('./phones.json');
+
+cors = require('cors')
 const app = express()
 const port = 3000
 
+app.use (cors());
+
 app.get('/', (req, res) => {
     res.send("Hello from my phone server")
+})
+app.get('/phones', (req, res) => {
+    res.send(phones)
+})
+app.get('/phones/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    console.log("I need phone for id ",id)
+    const phone = phones.find(phone => phone.id === id) || {};
+    res.send(phone)
 })
 
 app.listen(port, ()=> {
